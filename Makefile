867d01f405a733a7e7d2a9a2e9cf3e3e5b03fc48
tonytangdev <tony.tang.developer@gmail.com>
2026-03-29 22:25:29 +0200

    feat: wire tray, monitor, and cleaner in main

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 .PHONY: build run test clean
 
 build:
+	mkdir -p cmd/clearpaste/icons
+	cp icons/*.png cmd/clearpaste/icons/
 	go build -o bin/clearpaste ./cmd/clearpaste
 
 run:
+	mkdir -p cmd/clearpaste/icons
+	cp icons/*.png cmd/clearpaste/icons/
 	go run ./cmd/clearpaste
 
 test:
 	go test ./... -v
 
 clean:
-	rm -rf bin/
+	rm -rf bin/ cmd/clearpaste/icons/
--- cmd/clearpaste/main.go
+++ cmd/clearpaste/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+
+	"github.com/tonytangdev/clearpaste/internal/clipboard"
+	"github.com/tonytangdev/clearpaste/internal/tray"
+)
+
+//go:embed icons/icon.png
+var iconDefault []byte
+
+//go:embed icons/icon_active.png
+var iconActive []byte
+
+//go:embed icons/icon_disabled.png
+var iconDisabled []byte
 
 func main() {
-	fmt.Println("ClearPaste starting...")
+	// Inject icons into tray package
+	tray.IconDefault = iconDefault
+	tray.IconActive = iconActive
+	tray.IconDisabled = iconDisabled
+
+	cb := &clipboard.System{}
+	monitor := clipboard.NewMonitor(cb, cb)
+
+	stop := make(chan struct{})
+
+	monitor.OnCleaned = func() {
+		tray.FlashIcon()
+	}
+
+	// Start clipboard monitor in background
+	go monitor.Start(stop)
+
+	// Run tray (blocks until quit)
+	tray.Run(tray.Callbacks{
+		OnToggle: func(enabled bool) {
+			monitor.SetEnabled(enabled)
+		},
+		OnUndo: func() bool {
+			return monitor.Undo()
+		},
+		HasUndo: func() bool {
+			return monitor.HasUndo()
+		},
+	})
+
+	close(stop)
 }
